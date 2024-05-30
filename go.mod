a16d31a7d8b413702928b29023aa69fdf0d6dcdb
Phil Schumann <metaleap@users.noreply.github.com>
2024-05-30 14:13:30 +0200

    zui wip

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/metaleap/zui
 
 go 1.22.3
+
+require golang.org/x/net v0.25.0 // indirect
--- zui.go
+++ zui.go
@@ -2,15 +2,23 @@ package zui
 
 import (
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func FirstLineJS(zuiFileHash string) string {
 	return "//" + zuiFileHash + "\n"
 }
 
-func ToJS(zuiFileName string, zuiFileSrc string, zuiFileHash string) string {
+func ToJS(zuiFileName string, zuiFileSrc string, zuiFileHash string) (string, error) {
 	var buf strings.Builder
 	buf.WriteString(FirstLineJS(zuiFileHash))
 
-	return buf.String()
+	htm_root, err := html.Parse(strings.NewReader("<html>" + "</html>"))
+	if err != nil {
+		return "", err
+	}
+	_ = htm_root
+
+	return buf.String(), err
 }
